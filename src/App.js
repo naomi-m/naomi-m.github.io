7648dca640756320b98c3c53a30429b017a2c11e
Naomi <naomimakishi@gmail.com>
2024-12-12 18:55:35 -0500

    create react project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { indigo, amber } from '@mui/material/colors'
+import { createTheme } from "@mui/material/styles";
+
+import HomePage from './pages/HomePage';
+
+// customize look
+export const theme = createTheme({
+  palette: {
+    primary: indigo,
+    secondary: amber,
+  },
+});
+
+// App is the root component of our application and as children contain all our pages
+// We use React Router's BrowserRouter and Routes components to define the pages for
+// our application, with each Route component representing a page and the common
+// NavBar component allowing us to navigate between pages (with hyperlinks)
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import { Container, Divider, Link } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+// const config = require('../config.json');
+
+export default function HomePage() {
+
+  return (
+    <Container>
+
+      <h2>portfolio website</h2>
+
+      <p>coded using JS</p>
+    </Container>
+  );
+};
