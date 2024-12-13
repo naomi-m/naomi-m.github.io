23376f21f2ee7d15145670c3ee8f4bc5e1a8cefc
Naomi <naomimakishi@gmail.com>
2024-12-12 23:19:23 -0500

    homepage.js width dynamic when resize window

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,33 @@
+import { useEffect, useState } from 'react';
 import { Container, Divider, Link } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'reasct-router-dom';
 
 // const config = require('../config.json');
 
 export default function HomePage() {
+  let [containerWidth, setContainerWidth] = useState(100);
+
+  
+  useEffect(() => {
+    function handleResize() {
+      setContainerWidth(window.innerWidth <= 768 ? 100 : 50);  //768 standard px for mobile
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  });
+
 
   // container formatting
   // const flexFormat = { display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly' };
-  const allFormat = { fontFamily: 'calibri light' };
+  const allFormat = { fontFamily: 'calibri light', width: `${containerWidth}%` };
   const nameFormat = { textAlign: 'center' };
-  const contentFormat = { width: '70%' };
+  const contentFormat = {};
   const pFormat = { margin: 0, padding: 0 }
   const fourSpaces = "\u00A0\u00A0\u00A0\u00A0";  // unicode representation of non-breaking space '&nbsp;'
   const footerFormat = { fontSize: 10, textAlign: 'center' };
 
 
+  // functions
   //TODO function that parses education/projects first line
 
   /**
@@ -25,14 +38,16 @@ export default function HomePage() {
    */
   function skillLine(line) {
     // slice indexes
-    let hyphLoc = 0;
     let colonLoc = line.indexOf(':');
     // slice
-    let category = line.slice(hyphLoc+2, colonLoc);
+    let category = line.slice(2, colonLoc);  // hypen, space always at start so start at 2
     let description = line.slice(colonLoc+1);
     
     return <p style={pFormat}><b>{category}</b>: {description}</p>;
   }
+
+
+  // call functions
   let ts1 = skillLine("– Preferred Languages: Java, Python");
   let ts2 = skillLine("– Language Exposure: C++, C, SQL, MySQL, JavaScript, HTML, CSS");
   let ts3 = skillLine("– Competencies: Internet & Web Systems (Java, AWS EC2), Networked Systems (C++, Docker), Database & Information Systems (MySQL, JavaScript), Computer & Network Security (Python, JavaScript, VirtualBox)");
@@ -43,7 +58,7 @@ export default function HomePage() {
       <Container><br/><br/></Container>
       
       <Container style={nameFormat}>
-        <h1>Naomi Makishi</h1>
+        <h1>Naomi Makishi {containerWidth}</h1>
         <p>testtestestasf;l akjsdflkj aioiuawer haksjfan nvmvmaiue woi yatyhqljkfas kljfaoi ueouop qwiet ypqwe fk sjdnvnzx cmvlkflkj aioiuawer haksjfan nvmvmaiue woi yatyhqljkfas kljfaoi ueouop qwiet ypqwe fk sjdnvnzx cmvlkja dfoja dfoi</p>
       </Container>
       
